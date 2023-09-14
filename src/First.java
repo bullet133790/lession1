aecb32b209bb911cd331670a7ed392613365c61e
shter <shterebandrew@gmail.com>
2023-09-14 21:02:28 +0300

    firts commit

---
+++ src/First.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("say your name ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name);
+        System.out.println("today is bad weather");
+    }
+}
